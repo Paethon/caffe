23ea10552aeaeddec73529217e61703d446b1fc4
Sebastian Stabinger <c7031105@uibk.ac.at>
2016-07-22 14:49:01 +0200

    Working random layer

--- include/caffe/layers/random_layer.hpp
+++ include/caffe/layers/random_layer.hpp
@@ -3,6 +3,10 @@
 
 #include <vector>
 
+#include <boost/random.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/math/distributions/normal.hpp>
+
 #include "caffe/blob.hpp"
 #include "caffe/layer.hpp"
 #include "caffe/proto/caffe.pb.h"
@@ -19,9 +23,15 @@ public:
   explicit RandomLayer(const LayerParameter &param)
       : NeuronLayer<Dtype>(param) {}
 
+  virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                          const vector<Blob<Dtype> *> &top);
+
   virtual inline const char *type() const { return "Random"; }
 
 protected:
+  boost::mt19937 rng;
+  boost::random::uniform_int_distribution<> dist;
+
   virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
                            const vector<Blob<Dtype> *> &top);
 
--- src/caffe/layers/random_layer.cpp
+++ src/caffe/layers/random_layer.cpp
@@ -6,13 +6,20 @@
 namespace caffe {
 
 template <typename Dtype>
+void RandomLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                                    const vector<Blob<Dtype> *> &top) {
+  dist = boost::random::uniform_int_distribution<>(-1000, 1000);
+  return;
+}
+
+template <typename Dtype>
 void RandomLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype> *> &bottom,
                                      const vector<Blob<Dtype> *> &top) {
   const Dtype *bottom_data = bottom[0]->cpu_data(); // Incoming
   Dtype *top_data = top[0]->mutable_cpu_data();     // Outgoing
   const int count = bottom[0]->count();             // Number of values
   for (int i = 0; i < count; ++i) {
-    top_data[i] = std::max(bottom_data[i], Dtype(0));
+    top_data[i] = bottom_data[i] + dist(rng)/1000;
   }
 }
 
@@ -21,13 +28,12 @@ void RandomLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype> *> &top,
                                       const vector<bool> &propagate_down,
                                       const vector<Blob<Dtype> *> &bottom) {
   if (propagate_down[0]) {
-    const Dtype *bottom_data = bottom[0]->cpu_data();
     const Dtype *top_diff = top[0]->cpu_diff();
     Dtype *bottom_diff = bottom[0]->mutable_cpu_diff();
     const int count = bottom[0]->count();
     for (int i = 0; i < count; ++i) {
-      // If incoming value is negative set gradient to zero
-      bottom_diff[i] = top_diff[i] * (bottom_data[i] > 0);
+      // Just copy the incoming gradient since we only add random values
+      bottom_diff[i] = top_diff[i];
     }
   }
 }
@@ -37,4 +43,5 @@ STUB_GPU(RandomLayer);
 #endif
 
 INSTANTIATE_CLASS(RandomLayer);
+REGISTER_LAYER_CLASS(Random);
 } // namespace caffe
